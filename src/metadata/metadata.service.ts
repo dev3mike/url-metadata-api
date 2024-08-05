e603cdad1ec1abad21375aef6837f0135254f251
Masoud Banimahd <masoud.banimahd@polestar.com>
2024-08-05 10:27:36 +0200

    update user agent

--- src/metadata/metadata.service.ts
+++ src/metadata/metadata.service.ts
@@ -5,7 +5,12 @@ import * as urlMetadata from 'url-metadata';
 export class MetadataService {
     async getUrlMetadata(url: string) {
         try {
-            const metadata = await urlMetadata(url);
+            const metadata = await urlMetadata(url, {
+                requestHeaders: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                },
+            });
+
             return metadata;
         } catch (err) {
             console.error({ url, error: err });
